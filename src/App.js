7935628df3da8218177283c6fabd91d9dc6d87d9
swatinaik2003 <swati622005@gmail.com>
2024-09-21 14:31:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div>todo-list</div>
-    );
+    <div className="App" style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      {/* Header */}
+      <header className="header">
+        <h1>To-do List Application</h1>
+      </header>
+
+     
+
+      
+    </div>
+  );
 }
 
 export default App;
