1f3812ff440744ccabab957ecb98fb9c69a3bd9e
swatinaik2003 <swati622005@gmail.com>
2024-09-25 09:48:55 +0530

    2nd commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Todo List Application</h1>
+    </header>
+  );
+}
+
+export default Header;
