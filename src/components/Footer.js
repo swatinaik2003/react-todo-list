7b7326a65ee0b82e9334d28e95172a6749829cc8
swatinaik2003 <swati622005@gmail.com>
2024-09-25 10:50:34 +0530

    6th commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+ import React, { useState } from "react";
 
 function Footer({ addTask, tasks, setTasks }) {
   const [taskName, setTaskName] = useState("");
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,43 @@
-import React from "react";
+ import React from "react";
 
 function Main({ tasks, handleTaskChange }) {
+  const incompleteTasks = tasks.filter((task) => !task.completed);
+  const completedTasks = tasks.filter((task) => task.completed);
+
   return (
     <main className="main">
-      {tasks.map((task) => (
-        <div key={task.id}>
-          <input
-            type="checkbox"
-            checked={task.completed}
-            onChange={() => handleTaskChange(task.id)}
-          />
-          {task.name}
-        </div>
-      ))}
+      <h2>Incompleted Tasks</h2>
+      {incompleteTasks.length === 0 ? (
+        <p>No incomplete tasks</p>
+      ) : (
+        incompleteTasks.map((task) => (
+          <div key={task.id}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => handleTaskChange(task.id)}
+            />
+            {task.name}
+          </div>
+        ))
+      )}
+
+      <h2>Completed Tasks</h2>
+      {completedTasks.length === 0 ? (
+        <p>No completed tasks</p>
+      ) : (
+        completedTasks.map((task) => (
+          <div key={task.id}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => handleTaskChange(task.id)}
+              readOnly
+            />
+            <span style={{ textDecoration: "line-through" }}>{task.name}</span>
+          </div>
+        ))
+      )}
     </main>
   );
 }
